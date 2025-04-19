21d48f272251edce8caa2cc9b7de58979bdd9c80
Thor Tudal Lauridsen <30320530+REXKrash@users.noreply.github.com>
2025-04-19 18:34:02 +0200

    test: Minor improvement to CustomerRouteTest

--- apps/api/src/test/kotlin/com/github/thorlauridsen/CustomerRouteTest.kt
+++ apps/api/src/test/kotlin/com/github/thorlauridsen/CustomerRouteTest.kt
@@ -12,12 +12,12 @@ import io.ktor.http.contentType
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.testing.testApplication
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import java.util.UUID
-import kotlin.test.assertEquals
 
 class CustomerRouteTest {
 
@@ -28,14 +28,16 @@ class CustomerRouteTest {
     }
 
     @Test
-    fun `get customer - random id - returns not found`() = testApplication {
-        environment {
-            config = ApplicationConfig("application.yaml")
-        }
-        val nonExistentId = UUID.randomUUID()
-        val response = client.get("/customers/$nonExistentId")
+    fun `get customer - random id - returns not found`() {
+        testApplication {
+            environment {
+                config = ApplicationConfig("application.yaml")
+            }
+            val nonExistentId = UUID.randomUUID()
+            val response = client.get("/customers/$nonExistentId")
 
-        assertEquals(HttpStatusCode.NotFound, response.status)
+            assertEquals(HttpStatusCode.NotFound, response.status)
+        }
     }
 
     @ParameterizedTest
@@ -45,29 +47,31 @@ class CustomerRouteTest {
             "bob@gmail.com",
         ]
     )
-    fun `post customer - get customer - success`(mail: String) = testApplication {
-        environment {
-            config = ApplicationConfig("application.yaml")
+    fun `post customer - get customer - success`(mail: String) {
+        testApplication {
+            environment {
+                config = ApplicationConfig("application.yaml")
+            }
+
+            val customerInput = CustomerInputDto(mail)
+            val customerInputJson = json.encodeToString(customerInput)
+
+            val response = client.post("/customers") {
+                contentType(ContentType.Application.Json)
+                setBody(customerInputJson)
+            }
+
+            assertEquals(HttpStatusCode.Created, response.status)
+            val responseJson = response.bodyAsText()
+            val createdCustomer = json.decodeFromString<CustomerDto>(responseJson)
+            assertCustomer(createdCustomer, mail)
+
+            val response2 = client.get("/customers/${createdCustomer.id}")
+            assertEquals(HttpStatusCode.OK, response2.status)
+            val responseJson2 = response2.bodyAsText()
+            val fetchedCustomer = json.decodeFromString<CustomerDto>(responseJson2)
+            assertCustomer(fetchedCustomer, mail)
         }
-
-        val customerInput = CustomerInputDto(mail)
-        val customerInputJson = json.encodeToString(customerInput)
-
-        val response = client.post("/customers") {
-            contentType(ContentType.Application.Json)
-            setBody(customerInputJson)
-        }
-
-        assertEquals(HttpStatusCode.Created, response.status)
-        val responseJson = response.bodyAsText()
-        val createdCustomer = json.decodeFromString<CustomerDto>(responseJson)
-        assertCustomer(createdCustomer, mail)
-
-        val response2 = client.get("/customers/${createdCustomer.id}")
-        assertEquals(HttpStatusCode.OK, response2.status)
-        val responseJson2 = response2.bodyAsText()
-        val fetchedCustomer = json.decodeFromString<CustomerDto>(responseJson2)
-        assertCustomer(fetchedCustomer, mail)
     }
 
     /**
@@ -77,8 +81,8 @@ class CustomerRouteTest {
      * @param expectedMail Expected mail of the customer.
      */
     private fun assertCustomer(customer: CustomerDto, expectedMail: String) {
-        Assertions.assertNotNull(customer)
-        Assertions.assertNotNull(customer.id)
-        Assertions.assertEquals(expectedMail, customer.mail)
+        assertNotNull(customer)
+        assertNotNull(customer.id)
+        assertEquals(expectedMail, customer.mail)
     }
 }
